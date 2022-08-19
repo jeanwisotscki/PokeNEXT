45a415b81691502fe257b4a09f04c72053767b67
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-08-19 14:57:36 -0300

    new: criando pagina home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,46 @@ import type { NextPage } from "next";
 
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+interface IPokemon {
+  name: string;
+  url: string;
+  id: null | undefined | number;
+}
+
+export const getStaticProps = async () => {
+  const maxPokemons = 251;
+  const url = "https://pokeapi.co/api/v2/pokemon";
+
+  const res = await fetch(`${url}?limit=${maxPokemons}`);
+  const data = await res.json();
+
+  // add índice dos pokemons
+  data.results.forEach((pokemon: IPokemon, index: number) => {
+    pokemon.id = index + 1;
+  });
+
+  return {
+    props: { pokemons: data.results },
+  };
+};
+
+interface IPokemonsProps {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: IPokemon[];
+}
+
+const Home: NextPage<IPokemon[]> = ({ pokemons }: IPokemon[]) => {
   return (
     <>
-      <h1>Hello</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            {pokemon.id} - {pokemon.name}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
