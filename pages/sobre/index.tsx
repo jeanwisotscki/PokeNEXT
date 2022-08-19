9ebf7d7c247a8f4004dd1719a149eeaee9e73b59
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-08-19 08:23:53 -0300

    styled: página sobre estilizada

--- pages/sobre/index.tsx
+++ pages/sobre/index.tsx
@@ -4,18 +4,20 @@ import styles from "./index.module.css";
 
 const Sobre = () => {
   return (
-    <div>
+    <div className={styles.sobre}>
       <h2>Sobre o projeto</h2>
       <p>
         Desenvolvido como projeto de estudo, usando majoritariamente o framework
         Next.js e a linguagem Typescript.
       </p>
-      <Image
-        src="/images/charizard.png"
-        width="100%"
-        height="100%"
-        alt="pokemon charizard"
-      />
+      <div className={styles.img_container}>
+        <Image
+          src="/images/charizard.png"
+          width="300"
+          height="300"
+          alt="pokemon charizard"
+        />
+      </div>
     </div>
   );
 };
