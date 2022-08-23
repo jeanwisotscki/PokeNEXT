49d11c3586a749dd1c202edb7596c2ad1554095b
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-08-23 10:01:53 -0300

    refactor: componente search bar

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,15 +1,38 @@
+import React from "react";
+
 import styles from "./index.module.css";
 
 const SearchBar = () => {
+  const [value, setValue] = React.useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (value && +value <= 1154) {
+      setValue("");
+      return (window.location.href = `/pokemon/${value}`);
+    }
+
+    if (value && +value > 1154) {
+      setValue("");
+      return alert("O número máximo de pokemons é de 1154");
+    }
+
+    return alert("Preencha o campo corretamente");
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label htmlFor="search">Pesquisar pokemon</label>
       <input
-        type="text"
-        name=""
-        id=""
-        placeholder="Nome ou número do pokemon"
+        type="number"
+        name="search"
+        id="search"
+        placeholder="Número do pokemon"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
-      <button>pesquisar</button>
+      <button>Pesquisar</button>
     </form>
   );
 };
